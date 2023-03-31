4dd1a7438406c9077a3a8f191c317ed32541cd88
andifebrianto <andi.febrianto.pp@gmail.com>
2023-03-31 19:15:11 +0700

    create data with validation

--- app/Http/Controllers/API/FormController.php
+++ app/Http/Controllers/API/FormController.php
@@ -3,14 +3,25 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class FormController extends Controller
 {
-    public function index()
+    public function create(Request $request)
     {
+        $validatedData = $request->validate([
+            'nama' => 'required',
+            'alamat' => 'required',
+            'telepon' => 'required'
+        ]);
+
+        // dd($request->all());
+        Student::create($validatedData);
+
         return response()->json([
-            'message' => 'Ini halaman form'
+            'message' => 'Student berhasil ditambahkan',
+            'student' => $validatedData
         ], 200);
     }
 }
